32e08e5ebe7393203fb51fb1b2da087cb7bd4168
Tomer Addady <taddady@gmail.com>
2020-07-14 13:18:50 +0300

    server is done

---
+++ rest_sever.py
@@ -0,0 +1,28 @@
+import os
+import socket
+from uuid import uuid4
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+@app.route('/hello')
+def availability():
+    pod_name = socket.gethostname()
+    pod_ip = socket.gethostbyname(pod_name)
+    return 'hello user... this is openshift pod named: {}, on IP: {}'.format(pod_name, pod_ip)
+
+
+@app.route('/costum/<command>')
+def costum_command(command):
+    output_file = 'temp_output_{id}.txt'.format(id=uuid4().hex)
+    os.system('{command} > {output_file}'.format(command=command, output_file=output_file))
+    with open(output_file, 'r') as result_file:
+        result = result_file.read()
+    os.remove(output_file)
+    return result
+    
+
+if __name__ == '__main__':
+    app.run(port=80)
